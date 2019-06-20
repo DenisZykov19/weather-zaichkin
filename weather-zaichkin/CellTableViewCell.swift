f80f47b791bec4e9b33c73e037c14c790bb3a94c
denis <zdi@bk.ru>
2019-06-20 07:52:59 -0700

    20.06.19

---
+++ weather-zaichkin/CellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CellTableViewCell.swift
+//  weather-zaichkin
+//
+//  Created by WSR on 6/20/19.
+//  Copyright © 2019 WSR. All rights reserved.
+//
+
+import UIKit
+
+class CellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
