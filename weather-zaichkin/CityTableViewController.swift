0ee43890778651b542704f62fa45a1f79be4e1ab
denis <zdi@bk.ru>
2019-06-21 05:45:15 -0700

    21/06/19

--- weather-zaichkin/CityTableViewController.swift
+++ weather-zaichkin/CityTableViewController.swift
@@ -14,10 +14,14 @@ class CityTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Добавление первой записи в массив
         cityArray.append("Оренбург")
-        
+        cityArray.append("Москва")
+        cityArray.append("Санкт-Петербург")
+        cityArray.append("Томск")
+        //Изменение фона tableview
         tableView.backgroundView = UIImageView.init(image: UIImage(named:"Untitled"))
-        
+        //перерисовка tableview
         tableView.reloadData()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -27,26 +31,31 @@ class CityTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+    //Количество секций или групп ячеек. Минимум 1!
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    //Количество ячеек
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return cityArray.count
     }
 
-    
+    //Реализация самой ячейки
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     //Создание объекта ячейки из ячейки с идентификатором "cityCell" и классом CityTableViewCell
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! CellTableViewCell
-
+        //Присвоение значения label
         cell.label.text = cityArray[indexPath.row]
-
+        //Очистка цвета фона в ячейке, чтобы было видно картинку
+        cell.backgroundColor = UIColor.clear
         return cell
     }
-    
+    // Высота ячейки
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
 
     /*
     // Override to support conditional editing of the table view.
@@ -56,17 +65,20 @@ class CityTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            // Delete the row from the data source - Удаление из массива
+            cityArray.remove(at: indexPath.row)
+            //удаление из таблицы
             tableView.deleteRows(at: [indexPath], with: .fade)
+
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
@@ -83,14 +95,49 @@ class CityTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     //Обработка перехода
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         if segue.identifier == "toMain" {
+             //Создание объекта класса назначения, чтобы обратиться к переменной
+             let vc = segue.destination as! ViewController
+             //Получение индекса выбираемой ячейки
+             let index = self.tableView.indexPathForSelectedRow
+             //присвоение значения
+             vc.city = cityArray[(index?.row)!]
+         }
+     }
+     //Добавление нового города
+     @IBAction func addCity(_ sender: Any) {
+
+         //Создание объекта alert
+         let alert = UIAlertController(title: "Добавление город", message: "Добавьте пожалуйста город!", preferredStyle: .alert)
+         //Добавление поля для ввода
+         alert.addTextField { (textField) in
+     }
+
+        //Добавление действия в случае ОК
+     alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (alertAction) in
+         //Получаем значение которое ввел пользователь
+        //в алерте может быть несколько текстовых полей, поэтому нужно вытаскиваем по индексу
+        let textField = alert.textFields![0] as UITextField
+         //Добавление в массив
+        //в свой список городов добавляем новый элемент
+        self.cityArray.append(textField.text!)
+         // Перерисовка обновляем отображение
+         self.tableView.reloadData()
+         }))
+        //Добавление действия в случае Cancel
+     alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (alertAction) in
+     
+     }))
+         //Добавление alert на экран
+         self.present(alert, animated: true, completion: nil)
+         }
+
 
 }
+ 
--- weather-zaichkin/ViewController.swift
+++ weather-zaichkin/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
+
+
 
 class ViewController: UIViewController {
 
+
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+    @IBOutlet weak var templab: UILabel!
+    
+
+    var city = ""
+    var temp = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        downloadData(city: city)
+        cityNameLabel.text = city
     }
+    
+    
 
+    
+    
+    func downloadData(city: String) {
+       
+        // токен для АПИ
+        let token = "1e936ee21707e2a418e98dca00877357"
+        
+        // УРЛ с городом, метрической системой и токеном
+        let urlStr = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&units=metric&appid=\(token)"
+        
+        // перекодируем адрес (URLencode)
+        let url = urlStr.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        
+        // посылаем запрос
+        //см. https://cocoapods.org/pods/SwiftyJSON Work with Alamofire
+        Alamofire.request(url!, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                //если запрос выполнен успешно, то разбираем ответ и вытаскиваем нужные данные
+                let json = JSON(value)
+                self.temp = json["main"]["temp"].doubleValue
+                self.templab.text = json["main"]["temp"].stringValue+"°C"
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
 
